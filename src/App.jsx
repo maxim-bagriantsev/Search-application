cf8752c87747958097f3aaa475489c6a21ea919c
Maxim <maxim163rus@gmail.com>
2021-06-25 12:09:49 +0400

    component App only

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import React, {useEffect, useState} from 'react';
-import SearchSalary from './SearchSalary'
 import s from './App.module.css' ;
 
 const App = (props) => {
@@ -8,18 +7,35 @@ const App = (props) => {
     const [filterName, setFilterName] = useState('')
     const [users, setUsers] = useState([]);
 
+    const [filterSalary, setFilterSalary] = useState('')
+    const [salary, setSalary] = useState([]);
+
     const handleChangeName = e => {
         setFilterName(e.target.value);
     };
 
+    const handleChangeSalary = e => {
+        setFilterSalary(e.target.value);
+    };
+
 
     useEffect(() => {
-        const results = arr.filter((person) => {
-                return person.name.toLowerCase().includes(filterName.toLowerCase())
-            }
-        );
-        setUsers(results);
-    }, [filterName]);
+        if (!isNaN(filterName)) {
+            const results = arr.filter((person) => {
+                    return person.name.toLowerCase().includes(filterName.toLowerCase())
+                }
+            );
+            setUsers(results);
+
+        }
+        else {
+            const results = arr.filter((person) => {
+                    return person.salary.includes(filterSalary.toLowerCase())
+                }
+            );
+            setSalary(results);
+        }
+    }, [filterName], [filterSalary]);
 
     return (
         <div>
@@ -30,11 +46,21 @@ const App = (props) => {
                        placeholder='Search for surname'/>
                 <ul>
                     {users.map(item => (
-                        <li>{item.name}</li>
+                        <li key={item.id}>{item.name}</li>
                     ))}
                 </ul>
             </form>
-            <SearchSalary people={props.people}/>
+            <form>
+                <input type='text'
+                       value={filterSalary}
+                       onChange={handleChangeSalary}
+                       placeholder='Search for salary'/>
+                <ul>
+                    {salary.map(item => (
+                        <li key={item.id}>{item.salary}</li>
+                    ))}
+                </ul>
+            </form>
         </div>
     )
 }
